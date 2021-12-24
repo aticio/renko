8adc6f9b43c2b67f1b49ec4089ff87f85978fded
aticio <aticiozgur@gmail.com>
2021-12-24 17:55:05 +0300

    renko creation and brick addition according to new prices.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="renko",
-    version="0.0.1",
+    version="0.0.2",
     description="Renko chart creator.",
     py_modules=["renko"],
     classifiers=[
