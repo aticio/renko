4e13010b376e97899fc1d8108d3d0caa53b49150
aticio <aticiozgur@gmail.com>
2022-01-22 22:19:59 +0300

    readme update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="renko",
-    version="0.0.11",
+    version="0.0.12",
     description="Renko chart creator.",
     py_modules=["renko"],
     classifiers=[
